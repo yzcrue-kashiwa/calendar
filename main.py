d23e5190c1bc5ca9f524ff68b95cbca892b41a51
yzcrue-kashiwa <yz.crue@gmail.com>
2026-03-19 19:06:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -55,18 +55,4 @@ def main():
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
-    main()                        events.append({
-                            "date": current_date,
-                            "type": t
-                        })
-
-    return events
-
-def main():
-    data = scrape()
-
-    with open("data.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
-
-if __name__ == "__main__":
     main()
